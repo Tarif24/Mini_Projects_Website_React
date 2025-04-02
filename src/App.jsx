8fe0106475e6fe24984cd649cb876ec848bb32c7
Tarif24 <tarif24@hotmail.com>
2025-04-02 19:31:40 -0400

    created a main layout and a home page and added the routes to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
-    return (
-        <>
-            <h1>Mini_Projects_Website_React</h1>
-        </>
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<HomePage />} />
+            </Route>
+        )
     );
+
+    return <RouterProvider router={router} />;
 }
 
 export default App;
