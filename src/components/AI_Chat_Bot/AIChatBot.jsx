b61ecae0b2c6712c3af5121bfdf402bcb90ab124
Tarif24 <tarif24@hotmail.com>
2025-04-09 01:03:07 -0400

    made the input into a form and used usestate to keep track of the value

--- src/components/AI_Chat_Bot/AIChatBot.jsx
+++ src/components/AI_Chat_Bot/AIChatBot.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AIChatBot = () => {
+    const [inputText, setInputText] = useState("");
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        console.log("Input Text:", inputText);
+    };
+
     return (
         <>
             <div className="flex justify-center items-center h-[90vh]">
-                <div className="my-16 flex flex-wrap justify-center items-center gap-8 bg-gray-200 w-full h-full p-8 border-24 border-white">
-                    <div className="flex justify-center items-end border-3 rounded-2xl h-[90%] w-[90%]">
-                        <div className="relative h-[3rem] w-full border-2 border-black rounded-[5rem] mb-4 mx-4">
-                            <input
-                                type="text"
-                                placeholder="What do you want to know..."
-                                className="relative h-full p-3 w-full focus:outline-none rounded-[5rem]"
-                            />
-                            <button className="absolute right-0 top-0 h-full bg-blue-500 text-white rounded-[5rem] px-4 py-2">
-                                Send
-                            </button>
+                <div className="flex justify-center items-center gap-8 bg-gray-200 w-full h-full p-8 border-24 border-white">
+                    <form
+                        className="flex justify-center items-center w-full h-full"
+                        onSubmit={submitForm}
+                    >
+                        <div className="flex justify-center items-end border-3 rounded-2xl h-[90%] w-[90%] bg-white">
+                            <div className="relative h-[3rem] w-full border-2 border-black rounded-[5rem] mb-4 mx-4">
+                                <input
+                                    type="text"
+                                    placeholder="What do you want to know..."
+                                    className="relative h-full px-5 w-full focus:outline-none rounded-[5rem]"
+                                    onChange={(e) =>
+                                        setInputText(e.target.value)
+                                    }
+                                />
+                                <button
+                                    className="absolute right-0 top-0 h-full bg-blue-500 text-white rounded-[5rem] px-4 py-2"
+                                    type="submit"
+                                >
+                                    Send
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </>
